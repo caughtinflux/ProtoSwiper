def05b2a9c7cd095bbd6f7f3ee19ff76ba2ec69c
Aditya KD <aditya@adityakd.me>
2015-03-04 18:52:46 +0530

    Update default values.

--- ProtoSwiper/SwiperControlBehavior.m
+++ ProtoSwiper/SwiperControlBehavior.m
@@ -12,18 +12,18 @@
 - (instancetype)initWithItem:(id<UIDynamicItem>)item attachedToItem:(id<UIDynamicItem>)attachedItem pullDirection:(CGVector)direction {
     if ((self = [super init])) {
         UIAttachmentBehavior *attch = [[UIAttachmentBehavior alloc] initWithItem:item attachedToItem:attachedItem];
-        FBTweakBind(attch, length, @"Adjustments", @"Curve control", @"attachment length", 250.0);
+        FBTweakBind(attch, length, @"Adjustments", @"Curve control", @"attachment length", 300.0);
         [self addChildBehavior:attch];
         
         UIGravityBehavior *gravity = [[UIGravityBehavior alloc] initWithItems:@[item]];
-        FBTweakBind(gravity, magnitude, @"Adjustments", @"Physics", @"gravity", 1.5);
+        FBTweakBind(gravity, magnitude, @"Adjustments", @"Physics", @"gravity", 88.0);
         gravity.gravityDirection = direction;
         [self addChildBehavior:gravity];
         
         UIDynamicItemBehavior *ib = [[UIDynamicItemBehavior alloc] initWithItems:@[item]];
-        FBTweakBind(ib, density, @"Adjustments", @"Physics", @"square density", 1.0);
-        FBTweakBind(ib, friction, @"Adjustments", @"Physics", @"square friction", 0.5);
-        FBTweakBind(ib, resistance, @"Adjustments", @"Physics", @"square resistance", 1.0);
+        FBTweakBind(ib, density, @"Adjustments", @"Physics", @"square density", 5.0);
+        FBTweakBind(ib, friction, @"Adjustments", @"Physics", @"square friction", 42.0);
+        FBTweakBind(ib, resistance, @"Adjustments", @"Physics", @"square resistance", 23.00);
         [self addChildBehavior:ib];
     }
     return self;
