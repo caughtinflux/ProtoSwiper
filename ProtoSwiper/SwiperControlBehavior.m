1d59faa63725f877a473a543158e8daf5a6a7bd3
Aditya KD <aditya@adityakd.me>
2015-03-04 17:58:10 +0530

    Make swipe control behaviour tweakable

--- ProtoSwiper/SwiperControlBehavior.m
+++ ProtoSwiper/SwiperControlBehavior.m
@@ -12,18 +12,19 @@
 - (instancetype)initWithItem:(id<UIDynamicItem>)item attachedToItem:(id<UIDynamicItem>)attachedItem pullDirection:(CGVector)direction {
     if ((self = [super init])) {
         UIAttachmentBehavior *attch = [[UIAttachmentBehavior alloc] initWithItem:item attachedToItem:attachedItem];
-        attch.length = 250.0;
+        FBTweakBind(attch, length, @"Adjustments", @"Curve control", @"attachment length", 250.0);
         [self addChildBehavior:attch];
         
         UIGravityBehavior *gravity = [[UIGravityBehavior alloc] initWithItems:@[item]];
+        FBTweakBind(gravity, magnitude, @"Adjustments", @"Physics", @"gravity", 1.5);
         gravity.gravityDirection = direction;
         [self addChildBehavior:gravity];
         
         UIDynamicItemBehavior *ib = [[UIDynamicItemBehavior alloc] initWithItems:@[item]];
-        ib.friction = 0.5;
-        ib.resistance = 2.0;
+        FBTweakBind(ib, density, @"Adjustments", @"Physics", @"square density", 1.0);
+        FBTweakBind(ib, friction, @"Adjustments", @"Physics", @"square friction", 0.5);
+        FBTweakBind(ib, resistance, @"Adjustments", @"Physics", @"square resistance", 1.0);
         [self addChildBehavior:ib];
-        
     }
     return self;
 }
